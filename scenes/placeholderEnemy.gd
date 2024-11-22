d6a1b3c50292e90d7e448acf481e81c6e357d1b4
Quill-x <quillxgaming222@gmail.com>
2024-11-21 21:29:22 -0800

    Added Ability to Get Hit

--- scenes/placeholderEnemy.gd
+++ scenes/placeholderEnemy.gd
@@ -1,7 +1,14 @@
 extends CharacterBody2D
+class_name Enemy
 
 const SPEED = 200.0
 
 func _physics_process(delta: float) -> void:
 	velocity.x = -SPEED
 	move_and_slide()
+
+
+func _on_area_entered(area):
+	if area.get_parent() is Player:
+		print("test")
+		area.get_parent().get_hit(position)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,50 +1,77 @@
 extends CharacterBody2D
+class_name Player
 
 const MAXSPEED = 700
 const ACCELERATION = 500
 const FRICTION = 150
+const HIT_VELOCITY = 5000
+const HIT_SLOWDOWN_RATE = 0.8  # Slowdown factor per frame when in "Hit" state (e.g., 0.95 is a 5% per frame slowdown)
+
+var rotational_velocity = 0
 var maxspeed = MAXSPEED
+var state = "Normal"
+
+func get_hit(pos):
+	state = "Hit"
+	velocity.x = 0
+	velocity.y = 0
+	$Hit.pitch_scale = RandomNumberGenerator.new().randf_range(0.7, .9)
+	print($Hit.pitch_scale)
+	$Hit.play()
+	var direction = (position - pos).normalized()
+	velocity = direction * HIT_VELOCITY
+	rotational_velocity = 200
+	var timer = await get_tree().create_timer(0.2).timeout
+	state = "Normal"
+	rotational_velocity = 0
+	rotation = 0
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("Left"):
-		velocity.x -= ACCELERATION
-	if Input.is_action_pressed("Right"):
-		velocity.x += ACCELERATION
-	if Input.is_action_pressed("Up"):
-		velocity.y -= ACCELERATION
-	if Input.is_action_pressed("Down"):
-		velocity.y += ACCELERATION
+	if state == "Normal":
+		# Normal movement logic
+		if Input.is_action_pressed("Left"):
+			velocity.x -= ACCELERATION
+		if Input.is_action_pressed("Right"):
+			velocity.x += ACCELERATION
+		if Input.is_action_pressed("Up"):
+			velocity.y -= ACCELERATION
+		if Input.is_action_pressed("Down"):
+			velocity.y += ACCELERATION
 
-	var direction_x = Input.get_axis("Left", "Right")
-	var direction_y = Input.get_axis("Up", "Down")
-	
-	if direction_x != 0:
-		var speed_factor_x = 1 - abs(velocity.x / maxspeed) + 0.2
-		velocity.x += direction_x * ACCELERATION * speed_factor_x
-	else:
-		if abs(velocity.x) > FRICTION:
-			velocity.x -= sign(velocity.x) * FRICTION
+		var direction_x = Input.get_axis("Left", "Right")
+		var direction_y = Input.get_axis("Up", "Down")
+		
+		if direction_x != 0:
+			var speed_factor_x = 1 - abs(velocity.x / maxspeed) + 0.2
+			velocity.x += direction_x * ACCELERATION * speed_factor_x
 		else:
-			velocity.x = 0
+			if abs(velocity.x) > FRICTION:
+				velocity.x -= sign(velocity.x) * FRICTION
+			else:
+				velocity.x = 0
 
-	if direction_y != 0:
-		var speed_factor_y = 1 - abs(velocity.y / maxspeed) + 0.2
-		velocity.y += direction_y * ACCELERATION * speed_factor_y
-	else:
-		if abs(velocity.y) > FRICTION:
-			velocity.y -= sign(velocity.y) * FRICTION
+		if direction_y != 0:
+			var speed_factor_y = 1 - abs(velocity.y / maxspeed) + 0.2
+			velocity.y += direction_y * ACCELERATION * speed_factor_y
 		else:
-			velocity.y = 0
+			if abs(velocity.y) > FRICTION:
+				velocity.y -= sign(velocity.y) * FRICTION
+			else:
+				velocity.y = 0
 
-	if Input.is_action_pressed("Slow"):
-		maxspeed = MAXSPEED / 2
-	else:
-		maxspeed = MAXSPEED
+		if Input.is_action_pressed("Slow"):
+			maxspeed = MAXSPEED / 2
+		else:
+			maxspeed = MAXSPEED
 
-	if velocity.length() > maxspeed:
-		velocity = velocity.normalized() * maxspeed
+		if velocity.length() > maxspeed:
+			velocity = velocity.normalized() * maxspeed
+	elif state == "Hit":
+		velocity *= HIT_SLOWDOWN_RATE
 
 	$AnimatedSprite2D.scale.y = 2 - abs(velocity.x / (MAXSPEED * 4)) + abs(velocity.y / (MAXSPEED * 5))
 	$AnimatedSprite2D.scale.x = 2 - abs(velocity.y / (MAXSPEED * 4)) + abs(velocity.x / (MAXSPEED * 8))
 
+	rotation += rotational_velocity
+	
 	move_and_slide()
